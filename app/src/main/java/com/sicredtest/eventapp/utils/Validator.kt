0a374b20ae2ec0e106e9338304ab8979822f689a
IVAN MILLER MEDEIROS CARDOSO <immc.hef@uea.edu.br>
2020-12-20 22:53:23 -0400

    test: add viewmodel test

--- app/src/main/java/com/sicredtest/eventapp/utils/Validator.kt
+++ app/src/main/java/com/sicredtest/eventapp/utils/Validator.kt
@@ -2,5 +2,5 @@ package com.sicredtest.eventapp.utils
 
 import android.util.Patterns
 
-fun CharSequence.isValid() = !isNullOrEmpty() && !isNullOrBlank()
-fun CharSequence.isEmailValid() = isValid() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+fun CharSequence?.isValid() = !isNullOrEmpty() && !isNullOrBlank()
+fun CharSequence?.isEmailValid() = isValid() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
--- app/src/test/java/com/sicredtest/eventapp/view/event/viewmodel/EventViewModelTest.kt
+++ app/src/test/java/com/sicredtest/eventapp/view/event/viewmodel/EventViewModelTest.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.Observer
 import com.sicredtest.eventapp.data.model.CheckIn
 import com.sicredtest.eventapp.data.model.Event
 import com.sicredtest.eventapp.data.repository.EventRepository
+import com.sicredtest.eventapp.utils.MockitoHelper
+import com.sicredtest.eventapp.utils.isEmailValid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
@@ -17,8 +19,9 @@ import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
 @RunWith(JUnit4::class)
@@ -42,11 +45,68 @@ class EventViewModelTest {
     @Test
     fun fetchEvents_success() {
         val events: List<Event> = listOf(mock(Event::class.java), mock(Event::class.java))
-        Mockito.`when`(runBlocking { eventRepository.getEvents() }).thenReturn(events)
+        `when`(runBlocking { eventRepository.getEvents() }).thenReturn(events)
         val observer = mock(Observer::class.java) as Observer<in List<Event>?>
         eventViewModel.fetchEvents().observeForever(observer)
         assertNotNull(eventViewModel.eventsLiveData.value)
         assertEquals(eventViewModel.eventsLiveData.value, events)
+    }
+    @Test
+    fun fetchEvents_failure() {
+        `when`(runBlocking { eventRepository.getEvents() }).thenReturn(null)
+        val observer = mock(Observer::class.java) as Observer<in List<Event>?>
+        eventViewModel.fetchEvents().observeForever(observer)
+        assertEquals(eventViewModel.eventsLiveData.value, null)
+    }
+
+    @Test
+    fun checkIn_success() {
+        Mockito.`when`(runBlocking { eventRepository.eventCheckIn(MockitoHelper.anyObject()) }).thenReturn(true)
+        val observer = mock(Observer::class.java) as Observer<in Boolean?>
+        eventViewModel.isCheckInSuccess.observeForever(observer)
+        eventViewModel.isCheckInFailure.observeForever(observer)
+        eventViewModel.checkIn()
+        assertEquals(eventViewModel.isCheckInSuccess.value, true)
+        assertEquals(eventViewModel.isCheckInFailure.value, false)
+    }
+
+    @Test
+    fun checkIn_failure() {
+        Mockito.`when`(runBlocking { eventRepository.eventCheckIn(MockitoHelper.anyObject()) }).thenReturn(false)
+        val observer = mock(Observer::class.java) as Observer<in Boolean?>
+        eventViewModel.isCheckInSuccess.observeForever(observer)
+        eventViewModel.isCheckInFailure.observeForever(observer)
+        eventViewModel.checkIn()
+        assertEquals(eventViewModel.isCheckInSuccess.value, false)
+        assertEquals(eventViewModel.isCheckInFailure.value, true)
+    }
+
+    @Test
+    fun validate_Clear_Checkin() {
+        val observer = mock(Observer::class.java) as Observer<in Boolean?>
+        eventViewModel.isCheckInSuccess.observeForever(observer)
+        eventViewModel.isCheckInFailure.observeForever(observer)
+        eventViewModel.isEmailValid.observeForever(observer)
+        eventViewModel.isNameValid.observeForever(observer)
+        eventViewModel.clearCheckIn()
+        assertEquals(eventViewModel.isCheckInSuccess.value, false)
+        assertEquals(eventViewModel.isNameValid.value, false)
+        assertEquals(eventViewModel.isEmailValid.value, false)
+        assertEquals(eventViewModel.isCheckInFailure.value, false)
+    }
+
+    @Test
+    fun validate_set_selected_event() {
+        val selectedEvent = mock(Event::class.java)
+        val eventObserver = mock(Observer::class.java) as Observer<in Event?>
+        val checkInObserver = mock(Observer::class.java) as Observer<in CheckIn?>
+        eventViewModel.selectedEventLiveData.observeForever(eventObserver)
+        eventViewModel.checkInEventLiveData.observeForever(checkInObserver)
+        eventViewModel.setSelectedEvent(selectedEvent)
+        assertNotNull(eventViewModel.selectedEventLiveData.value)
+        assertEquals(eventViewModel.selectedEventLiveData.value, selectedEvent)
+        assertNotNull(eventViewModel.checkInEventLiveData.value)
+        assertEquals(eventViewModel.checkInEventLiveData.value?.eventId, selectedEvent.id)
     }
 
 }
